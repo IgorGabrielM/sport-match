88b94659008a3abf7e48d28d75a37faf5eb31af2
M493750 <@M493750>
2025-05-23 18:29:53 -0300

    remove env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "sport-match-5648d",
+      appId: "1:110627582997:web:8f2c04a2a7c3adb8b0ca1c",
+      storageBucket: "sport-match-5648d.firebasestorage.app",
+      apiKey: "AIzaSyDvsTP2HHart9CbG9C3mdOlwIBRyW1Dz78",
+      authDomain: "sport-match-5648d.firebaseapp.com",
+      messagingSenderId: "110627582997",
+      measurementId: "G-2JHTBC7BFH"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())]
 };
