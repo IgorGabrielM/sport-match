7db046cdf4dfc8e05de1338274e22446b0fdb219
igorGabrielM <igorgabprofissional@gmail.com>
2025-11-03 15:07:15 -0300

    removendo environments do versionamento

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  // **AVISO DE SEGURANÇA:** Não use isso em produção! A chave API será exposta no código-fonte do cliente.
-  geminiApiKey: 'AIzaSyDCXzekFVz4aEIc7NKlayikktnteJbOOjY',
-};
